db4116458c5e2b9819498dcebd04aee9cf769ac2
Rocky Zhang <rockyzhz@gmail.com>
2023-12-21 13:58:05 +0800

    初稿完成

--- l3doc-zh-cn/l3compile.bat
+++ l3doc-zh-cn/l3compile.bat
@@ -2,7 +2,7 @@ FOR /F "tokens=1 delims=." %%G IN ("%1") DO (
     xelatex %1
     xelatex %1
     makeindex -s gglo.ist -o %%G.gls %%G.glo
-    makeindex -s gind.ist -o %%G.ind %%G.idx
+    makeindex -s l3doc.ist -o %%G.ind %%G.idx
     xelatex %1
     xelatex %1
 )
